8fb222bac5c09dbec7357ace4fb6d7a6a8a3b496
t.moechel <t.moechel@FuturaSolutionsGmbH2019.onmicrosoft.com>
2020-11-14 17:47:18 +0100

    adding folders

---
+++ src/screens/indexScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+interface IProps {}
+
+const IndexScreen: React.FunctionComponent<IProps> = () => {
+  return <Text>Index Screen Test</Text>;
+};
+
+const style = StyleSheet.create({});
+
+export default IndexScreen;
